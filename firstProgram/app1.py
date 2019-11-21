940107efbdaf0776d3289cd5d7ea7cbdf342ad7a
ma <ma_yamaki@yahoo.com>
2019-11-21 20:25:37 +0900

    mm

--- firstProgram/app1.py
+++ firstProgram/app1.py
@@ -9,6 +9,8 @@ def translate(w):
         return data[w]
     elif w.title() in data:
         return data[w.title()]
+    elif w.upper() in data:
+        return  data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter if Yes, or No if No: " % get_close_matches(w, data.keys())[0])
         yn = yn.upper()
